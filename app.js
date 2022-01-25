42b46450180bc1f5931e5790d640be09d674c246
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-01-25 05:54:17 -0600

    finished sidebar project

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const toggleBtn = document.querySelector('.sidebar-toggle')
 const closeBtn = document.querySelector('.close-btn')
 const sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', function(){
+    // console.log(sidebar.classList);
+
+    // short way
+    sidebar.classList.toggle('show-sidebar')
+
+    // longer way
+    // if(sidebar.classList.contains('show-sidebar')){
+    //     sidebar.classList.remove('show-sidebar')
+    // }else{
+    //     sidebar.classList.add('show-sidebar')
+    // }
+})
+
+closeBtn.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar')
+})
